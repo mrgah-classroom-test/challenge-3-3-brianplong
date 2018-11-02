65c71b58006adb930b577ff9266e0612d3fa35eb
mrgah <omniadisce@gmail.com>
2018-11-02 15:20:32 -0400

    initial commit, w/ challenge

---
+++ challenge_3_3-test.py
@@ -0,0 +1,13 @@
+
+# Set out count variable
+count = 1
+
+# The while statement
+while count &lt; 3:
+    print(count, fish)
+    count += 1
+
+# Re-write this statement as an /else/
+# clause in the /while/ statement
+print(Red fishnBlue fish)
+
